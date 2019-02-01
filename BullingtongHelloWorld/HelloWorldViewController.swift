810e8384354bad211ee61c5b2b75220fc48b458d
graesonb123 <bullingtong@health.missouri.edu>
2019-01-31 22:53:27 -0600

    Implemented UI

---
+++ BullingtongHelloWorld/HelloWorldViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HelloWorldViewController.swift
+//  BullingtongHelloWorld
+//
+//  Created by Graeson Bullington on 1/31/19.
+//  Copyright © 2019 Graeson Bullington. All rights reserved.
+//
+
+import UIKit
+
+class HelloWorldViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
